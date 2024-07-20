0c31127049ab96c16f3835d9006913c6fccace14
Youness Bourgui <younessbourgui07@gmail.com>
2024-07-20 20:30:05 +0200

    Secure Endpoints

--- src/main/java/main/api/security/SecurityConfig.java
+++ src/main/java/main/api/security/SecurityConfig.java
@@ -4,11 +4,13 @@
  */
 package main.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 /**
  *
@@ -17,11 +19,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    public SecurityConfig(JWTRequestFilter filter) {
+        this.filter = filter;
+    }
+    private JWTRequestFilter filter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.addFilterBefore(filter, AuthorizationFilter.class);
         http
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()  // Permit access to all requests
+                    .requestMatchers("/api/products","/auth/register","/auth/login").permitAll()
+                .anyRequest().authenticated()  // Permit access to all requests
             )
             .csrf(csrf -> csrf.disable());  // Disable CSRF protection
 
